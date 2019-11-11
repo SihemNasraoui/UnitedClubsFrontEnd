312a358094bd1d55a55e4743dcd560341245bcba
ineskd <ineskd03@gmail.com>
2019-11-11 13:05:22 +0100

    update register page

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -45,9 +45,8 @@ class Login extends Component {
                 <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
                   <CardBody className="text-center">
                     <div>
-                      <h2>Sign up</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
+                      <h1>Sign Up</h1><br/>
+                      <h2> Let's find you the perfect events </h2>
                       <Link to="/register">
                         <Button color="primary" className="mt-3" active tabIndex={-1}>Register Now!</Button>
                       </Link>
--- src/views/Pages/Register/Register.js
+++ src/views/Pages/Register/Register.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
 
 class Register extends Component {
+
+
+  onchangeselect = (e) => {
+    let choix = document.getElementById('select').value;
+    if (choix === "Student") {
+      var Student = document.getElementById('Student');
+      Student.style.display = ''; }
+      else{
+      Student.style.display = "none";}
+    
+   
+
+  }
+
+
   render() {
     return (
       <div className="app flex-row align-items-center">
@@ -19,31 +34,125 @@ class Register extends Component {
                           <i className="icon-user"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="text" placeholder="Username" autoComplete="username" />
+                      <Input type="text" placeholder="Firstname" autoComplete="Firstname" /><Input type="text" placeholder="Lastname" autoComplete="Lastname" />
                     </InputGroup>
+
+
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-user"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="Pseudo" autoComplete="Pseudo" />
+                    </InputGroup>
+
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="fa fa-id-card-o"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="Cin" autoComplete="Cin" />
+                    </InputGroup>
+
+
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>@</InputGroupText>
                       </InputGroupAddon>
-                      <Input type="text" placeholder="Email" autoComplete="email" />
+                      <Input type="email" placeholder="Email" autoComplete="email" />
                     </InputGroup>
+
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>
                           <i className="icon-lock"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="password" placeholder="Password" autoComplete="new-password" />
+                      <Input type="password" placeholder="Password" autoComplete="new-password" />                       <Input type="password" placeholder="Repeat password" autoComplete="new-password" />
+
                     </InputGroup>
-                    <InputGroup className="mb-4">
+
+                    <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>
-                          <i className="icon-lock"></i>
+                          <i className="icon-phone"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="password" placeholder="Repeat password" autoComplete="new-password" />
+                      <Input type="text" placeholder="phone" autoComplete="phone" /><InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-location-pin"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="Address" autoComplete="Address" />
                     </InputGroup>
-                    <Button color="success" block>Create Account</Button>
+
+
+
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-calendar"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="date" />
+                    </InputGroup>
+
+
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="fa fa-venus-mars"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <span>Sexe   </span>
+                      <input type="checkbox" name="sexe" value="male" />  Male  <br />
+                      <input type="checkbox" name="sexe" value="female" />   Female<br />
+                    </InputGroup>
+
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-people"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="select" name="userrole" id="userrole" id="select" onChange={this.onchangeselect.bind(this)}>
+                        <option disabled selected>Select your role </option>
+                        <option value="Student">Student</option>
+                        <option value="Sponsor">Sponsor</option>
+                        <option value="Administration">Administration</option>
+                      </Input>
+                    </InputGroup>
+
+                    <script></script>
+
+
+                    <div style="display ='none'" id="Student">
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-user"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                  <Input type="text" placeholder="Niveau" autoComplete="Niveau" /><Input type="text" placeholder="Lastname" autoComplete="Lastname" />
+                      </InputGroup>
+                    </div>
+
+
+
+
+
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-camera"></i>
+                        </InputGroupText>
+                      </InputGroupAddon> <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" />
+                    </InputGroup>
+
+
+                    <Button color="info" block>Create Account</Button>
                   </Form>
                 </CardBody>
                 <CardFooter className="p-4">
