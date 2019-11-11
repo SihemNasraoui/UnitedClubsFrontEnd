65f9d94bee79e3330a713d4e16a9d69283e9bd32
ineskd <ineskd03@gmail.com>
2019-11-11 16:48:35 +0100

    update register page2

--- src/views/Pages/Register/Register.js
+++ src/views/Pages/Register/Register.js
@@ -1,20 +1,93 @@
 import React, { Component } from 'react';
 import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
 
+
 class Register extends Component {
 
+  onchangeselectspons = (x, y) => {
+    let choix = document.getElementById('selectspons').value;
+    var entreprisediv = document.getElementById('entreprisediv');
+
+    if (choix === "Personnel") {
+      entreprisediv.style.display = "none";
+      entreprisediv.style.height = "0";
+    } else {
+      if (entreprisediv.style.display === "") {
+        //Hidden entreprisediv
+        entreprisediv.style.display = "none";
+        entreprisediv.style.height = "0";
+      } else {
+        //Show entreprisediv
+        entreprisediv.style.display = "";
+        entreprisediv.style.height = "auto";
+      }
+    }
+  }
 
   onchangeselect = (e) => {
     let choix = document.getElementById('select').value;
-    if (choix === "Student") {
-      var Student = document.getElementById('Student');
-      Student.style.display = ''; }
-      else{
-      Student.style.display = "none";}
-    
-   
+    var studentdiv = document.getElementById('Studentdiv');
+    var sponsdiv = document.getElementById('sponsdiv');
+    var adminsdiv = document.getElementById('adminsdiv');
 
+    if (choix === "Student") {
+      if (studentdiv.style.display === "") {
+        //Hidden studentdiv
+        studentdiv.style.display = "none";
+        studentdiv.style.height = "0";			// Optionnel rétablir la hauteur
+      }
+      else {
+        //Hidden sponsdiv
+        sponsdiv.style.display = "none";
+        sponsdiv.style.height = "0";
+        //Hidden adminsdiv
+        adminsdiv.style.display = "none";
+        adminsdiv.style.height = "0";
+        //Show studentdiv
+        studentdiv.style.display = "";
+        studentdiv.style.height = "auto";			// Optionnel libérer l'espace
+      }
+
+
+    } else if (choix === "Sponsor") {
+      if (sponsdiv.style.display === "") {
+        //Hidden sponsdiv
+        sponsdiv.style.display = "none";
+        sponsdiv.style.height = "0";			// Optionnel rétablir la hauteur
+      }
+      else {
+        //Hidden studentdiv
+        studentdiv.style.display = "none";
+        studentdiv.style.height = "0";
+        //Hidden adminsdiv
+        adminsdiv.style.display = "none";
+        adminsdiv.style.height = "0";
+        //Show sponsdiv
+        sponsdiv.style.display = "";
+        sponsdiv.style.height = "auto";			// Optionnel libérer l'espace
+      }
+
+    } else {
+      if (adminsdiv.style.display === "") {
+        //Hidden adminsdiv
+        adminsdiv.style.display = "none";
+        adminsdiv.style.height = "0";			// Optionnel rétablir la hauteur
+      }
+      else {
+        //Hidden studentdiv
+        studentdiv.style.display = "none";
+        studentdiv.style.height = "0";
+        //Hidden sponsdiv
+        sponsdiv.style.display = "none";
+        sponsdiv.style.height = "0";
+        //Show adminsdiv
+        adminsdiv.style.display = "";
+        adminsdiv.style.height = "auto";			// Optionnel libérer l'espace
+      }
+    }
   }
+
+
 
 
   render() {
@@ -106,7 +179,7 @@ class Register extends Component {
                           <i className="fa fa-venus-mars"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <span>Sexe   </span>
+                      <span>Gender  </span>
                       <input type="checkbox" name="sexe" value="male" />  Male  <br />
                       <input type="checkbox" name="sexe" value="female" />   Female<br />
                     </InputGroup>
@@ -117,7 +190,7 @@ class Register extends Component {
                           <i className="icon-people"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="select" name="userrole" id="userrole" id="select" onChange={this.onchangeselect.bind(this)}>
+                      <Input type="select" name="userrole" id="select" onChange={this.onchangeselect.bind(this)}>
                         <option disabled selected>Select your role </option>
                         <option value="Student">Student</option>
                         <option value="Sponsor">Sponsor</option>
@@ -125,18 +198,81 @@ class Register extends Component {
                       </Input>
                     </InputGroup>
 
-                    <script></script>
 
 
-                    <div style="display ='none'" id="Student">
+
+                    <div id="Studentdiv" style={{ display: 'none' }} >
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-graduation"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="text" placeholder="University" autoComplete="University" />
+                      </InputGroup>
                       <InputGroup className="mb-3">
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
                             <i className="icon-user"></i>
                           </InputGroupText>
                         </InputGroupAddon>
-                  <Input type="text" placeholder="Niveau" autoComplete="Niveau" /><Input type="text" placeholder="Lastname" autoComplete="Lastname" />
+                        <Input type="text" placeholder="Level" autoComplete="Level" /><Input type="text" placeholder="Inscription Number" autoComplete="Inscription Number" />
                       </InputGroup>
+
+                    </div>
+
+
+                    <div id="sponsdiv" style={{ display: 'none' }} >
+                      <InputGroup>
+                        <Input type="select" name="sponstype" id="selectspons" onChange={this.onchangeselectspons.bind(this)}>
+                          <option value="Personnel">Personnel</option>
+                          <option value="Entreprise">Entreprise</option>
+                        </Input>
+                      </InputGroup>
+                    </div>
+
+                    <div id="entreprisediv" style={{ display: 'none' }} >
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-graduation"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="text" placeholder="Entreprise Name" autoComplete="Entreprise Name" /><Input type="text" placeholder="Matricule" autoComplete="Matricule" />
+                      </InputGroup>
+
+
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-graduation"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="text" placeholder="Function" autoComplete="Function" />
+                      </InputGroup>
+                    </div>
+
+
+
+
+                    <div id="adminsdiv" style={{ display: 'none' }} >
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-graduation"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="text" placeholder="University" autoComplete="University" />
+                      </InputGroup>
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-user"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="text" placeholder="Departement" autoComplete="Departement" /><Input type="text" placeholder="Fonction" autoComplete="Fonction" />
+                      </InputGroup>
+
                     </div>
 
 
